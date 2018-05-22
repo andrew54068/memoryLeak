4fdf24c0fdc535a1c78bb7031f7dab88a912c1c6
andrew54068 <andrew0424718012@yahoo.com.tw>
2018-05-22 23:38:47 +0800

    Initial Commit

---
+++ testForMemoryLeak/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  testForMemoryLeak
+//
+//  Created by kidnapper on 22/05/2018.
+//  Copyright © 2018 kidnapper. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
